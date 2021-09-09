f4adb111298af20f28f4474675adb11a65069046
hushuaichen <2639281637@qq.com>
2021-09-10 06:30:22 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <router-view/>
+    <Footer/>
+  </div>
+</template>
+<script>
+  import Footer from './components/Footer'
+  export default {
+    components:{
+      Footer
+    }
+  }
+</script>
+<style>
+
+</style>
